bca5cc474d60742e6174f864e4e33aaf49f85074
Jepoy <jefferson.claud@nuworks.ph>
2021-06-20 20:39:33 +0800

    using product config and installation of 3rd party libraries for ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,34 @@
-import {ProjectInput} from './components/project-input';
-import {ProjectList} from './components/project-list';
+import 'reflect-metadata';
+// import { plainToClass } from 'class-transformer';
+import { Product } from './product.model';
+import { validate } from 'class-validator';
 
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
+
+// let products = [
+//     {title: 'Book', price: 12.99}
+// ];
+
+
+const newProd = new Product('', -1.20);
+
+validate(newProd).then(errors => {
+    if (errors.length > 0) {
+        console.log('Validation ERRORS');
+        console.log(errors);
+    } else {
+        console.log(newProd.getInformation());
+
+    }
+});
+
+// let loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price);
+// });
+
+
+// for(const prod of loadedProducts) {
+//     console.log(prod.getInformation());
+
+// }
+
+// const p1 = new Product('Book', 199);
