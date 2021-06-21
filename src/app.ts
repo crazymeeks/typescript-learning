1be4d37c828806cffec1687741fff1d689210d03
Jepoy <jefferson.claud@nuworks.ph>
2021-06-21 17:19:12 +0800

    select-share-a-place

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,12 @@
-import 'reflect-metadata';
-// import { plainToClass } from 'class-transformer';
-import { Product } from './product.model';
-import { validate } from 'class-validator';
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+function searchAddress(event: Event) {
+    event.preventDefault();
 
-// let products = [
-//     {title: 'Book', price: 12.99}
-// ];
+    const enteredAddress = addressInput.value;
 
-
-const newProd = new Product('', -1.20);
-
-validate(newProd).then(errors => {
-    if (errors.length > 0) {
-        console.log('Validation ERRORS');
-        console.log(errors);
-    } else {
-        console.log(newProd.getInformation());
-
-    }
-});
-
-// let loadedProducts = products.map(prod => {
-//     return new Product(prod.title, prod.price);
-// });
-
-
-// for(const prod of loadedProducts) {
-//     console.log(prod.getInformation());
-
-// }
-
-// const p1 = new Product('Book', 199);
+    // send to google's api
+    
+}
+form.addEventListener('submit', searchAddress);
